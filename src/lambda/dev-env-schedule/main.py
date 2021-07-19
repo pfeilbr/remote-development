fc62793a12ae4748b2696de2bf5247ba9b1cbbd6
Brian Pfeil <brian.pfeil@gmail.com>
2021-07-19 16:21:22 +0000

    index out of range fix

--- src/lambda/dev-env-schedule/main.py
+++ src/lambda/dev-env-schedule/main.py
@@ -7,18 +7,24 @@ instance_ids = json.loads(os.environ.get("INSTANCE_IDS"))
 ec2 = boto3.client("ec2", region_name=region)
 sns = boto3.client("sns", region_name=region)
 
+
 def is_instance_running(instance_id):
     response = ec2.describe_instance_status(InstanceIds=[instance_id])
-    return response['InstanceStatuses'][0]['InstanceState']['Name'] == 'running'
+    try:
+        return response["InstanceStatuses"][0]["InstanceState"]["Name"] == "running"
+    except:
+        return False
+
 
 def instance_operation(event, context):
-    #operation = getattr(event, 'operation', 'stop_instances')
+    # operation = getattr(event, 'operation', 'stop_instances')
     for instance_id in instance_ids:
         if is_instance_running(instance_id):
             ec2.stop_instances(InstanceIds=[instance_id])
-            print("{} instance (instance_id={})".format('stop_instances',instance_id) )
+            print("{} instance (instance_id={})".format("stop_instances", instance_id))
             notify(event, context)
-    
+
+
 def notify(event, context):
     topic_arn = region = os.environ.get("NOTIFY_TOPIC_ARN")
     sns.publish(
